aa1590baf34aad181413409196147efeb7b14ab7
Cristian Kiss <cristian.kiss@nearform.com>
2016-01-04 12:04:52 +0200

    improved test coverage

--- test/rabbitmq-transport.test.js
+++ test/rabbitmq-transport.test.js
@@ -4,12 +4,12 @@
 var Test = require('seneca-transport-test')
 
 var Lab = require('lab')
-// var Code = require('code')
+var Code = require('code')
 
 var lab = exports.lab = Lab.script()
 var describe = lab.describe
 var it = lab.it
-// var expect = Code.expect
+var expect = Code.expect
 
 describe('rabbitmq-transport', function () {
   it('happy-any', function (done) {
@@ -18,5 +18,28 @@ describe('rabbitmq-transport', function () {
 
   it('happy-pin', function (done) {
     Test.foo_pintest('rabbitmq-transport', require, done, 'rabbitmq', -6379)
+  })
+
+  it('options', function (done) {
+    var a = require('seneca')({
+      log: 'silent',
+      timeout: 23555
+    })
+      .use('../rabbitmq-transport.js')
+
+    var so = a.options()
+    expect(so.timeout).to.exist()
+    expect(so.timeout).to.equal(23555)
+
+    var b = require('seneca')({
+      log: 'silent',
+      timeout: null
+    })
+      .use('../rabbitmq-transport.js')
+
+    so = b.options()
+    expect(so.timeout).to.not.exists()
+
+    done()
   })
 })
